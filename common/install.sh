79b2a0c25ebfb2424327692aea9a0536660b2fef
Ziad <62176660+InsertX2k@users.noreply.github.com>
2025-03-17 20:32:49 +0200

    install.sh: remove calls to function setup_native_libs
    removes calls to the monetization functions

--- common/install.sh
+++ common/install.sh
@@ -1,2 +1,3 @@
-setup_native_libs
-rm -rf "$MODPATH/dex2oat_optimizer.png"
+# here the author called a function to setup monetization libs
+# setup_native_libs
+# rm -rf "$MODPATH/dex2oat_optimizer.png"
